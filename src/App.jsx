4619308b1e7246f731ac3a169caf7472d4bfa9f2
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-07-15 11:04:17 +0200

    base installata

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
