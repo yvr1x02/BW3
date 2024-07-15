942ab9ead063fd2b847bac893b259d50afab73ba
Yuri Impellizzeri <yuriimpellizzeri02@gmail.com>
2024-07-15 15:11:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import Topbar from "./components/Topbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
-      <Topbar />
+      <Profile></Profile>
     </div>
   );
 }
