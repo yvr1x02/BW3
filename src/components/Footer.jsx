058cdbdbde50b3d1fef7fa565042cfe4384c6150
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-07-15 16:40:02 +0200

    struttura base

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,32 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Card from "react-bootstrap/Card";
-import { fetchProfile } from "../redux/reducers/profileSlice";
-import Button from "react-bootstrap/Button";
+import { Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  const dispatch = useDispatch();
-  const profileData = useSelector((state) => state.profile.data);
-  const profileStatus = useSelector((state) => state.profile.status);
-
-  useEffect(() => {
-    if (profileStatus === "idle") {
-      dispatch(fetchProfile());
-    }
-  }, [profileStatus, dispatch]);
-
-  if (profileStatus === "loading") {
-    return <div>Loading...</div>;
-  }
-
-  if (profileStatus === "failed") {
-    return <div>Error loading profile data.</div>;
-  }
-
   return (
-    <Card className="p-0 card-linkedin-secondary  mt-2">
-      {profileData && (
-        <>
-          <Card.Title>
-            {profileData.name + " " + profileData.surname}
-          </Card.Title>
-          <Card.Text>{profileData.title}</Card.Text>
-          <Card.Text>{profileData.bio}</Card.Text>
-          <Card.Text className="text-secondary">{profileData.area}</Card.Text>
-          <div>
-            <Button variant="primary" className="rounded-pill ">
-              Disponibile per
-            </Button>
-            <Button variant="outline-primary" className="rounded-pill mx-2 ">
-              Aggiungi sezione del profilo
-            </Button>
-          </div>
-        </>
-      )}
-    </Card>
+    <Col>
+      <Container className=" card-linkedin-secondary  mt-2">
+        <div>
+          <a
+            className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            href="#"
+          >
+            Underline opacity 0
+          </a>
+          <a
+            className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            href="#"
+          >
+            Underline opacity 0
+          </a>
+          <a
+            className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+            href="#"
+          >
+            Underline opacity 0
+          </a>
+        </div>
+      </Container>
+    </Col>
   );
 }
 
