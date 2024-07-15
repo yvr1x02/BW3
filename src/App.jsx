258abe4a9dabfc9938a218001f499c8a208f808d
Yuri Impellizzeri <yuriimpellizzeri02@gmail.com>
2024-07-15 11:05:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { Provider } from "react-redux";
 import "./App.css";
 
 function App() {
