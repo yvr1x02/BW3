e32c870aa6865e44b11a08efbe5e2953aa052e72
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-07-15 16:00:54 +0200

    struttura pagina profilo

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import Topbar from "./components/Topbar";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <Profile />
+      <Footer />
     </div>
   );
 }
