64abc59291b0c38a7960fdbf94433dac80efdae5
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-07-15 10:52:10 +0200

    base finita

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
