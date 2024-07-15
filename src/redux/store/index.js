5e1e7b15b0a1cf2bfefdbf433d694008fb664c48
AlexDiGioia <digioia.alex2001@gmail.com>
2024-07-15 15:07:40 +0200

    up

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import profileReducer from '../reducers/profileSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import profileReducer from "../reducers/profileSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     profile: profileReducer,
   },
 });
+
+export default store;
