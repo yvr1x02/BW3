09678835bd435fbb7e45e1c63f2a94ddb8cf361d
MattiaSusin <m.susin28@gmail.com>
2024-07-15 16:33:40 +0200

    Commit fin eprimo drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+/* import { BrowserRouter, Routes, Route } from "react-router-dom"; */
+/* import { Provider } from "react-redux"; */
 import "./App.css";
+import TopBar from "./components/TopBar";
+import ContentProfile from "./components/ContentProfile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return(
+    <BrowserRouter>
+    <TopBar />
+    {/* <Routes>
+      <Route path="/" element={} />
+    </Routes> */}
+    
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/ContentProfile.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class ContentProfile extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default ContentProfile;
