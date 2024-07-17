e9f4f3c442dcfff56a036d06f3313047a0b094f1
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-07-17 14:24:53 +0200

    root

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Notizie from "../components/Notizie";
+import Topbar from "../components/Topbar";
+function HomePage() {
+  return (
+    <>
+      <Topbar />
+      <Notizie />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import Topbar from "../components/Topbar";
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+import ChatBox from "../components/ChatBox";
+function ProfilePage() {
+  return (
+    <>
+      <Topbar />
+      <Profile />
+      <Footer />
+      <ChatBox />
+    </>
+  );
+}
+
+export default ProfilePage;
