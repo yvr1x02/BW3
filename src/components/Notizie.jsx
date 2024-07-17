53979fc3fc5f773052b8414be05044cc8cd262de
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-07-17 12:29:12 +0200

    add Notizie component

--- src/components/Notizie.jsx
+++ src/components/Notizie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../redux/reducers/postSlice";
 import Post from "./Post";
