fa7e846a3881e0efece060478481b196bebfb36c
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-07-18 17:45:13 +0200

    Update Jobs.jsx

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import { fetchJobs } from "../redux/reducers/jobsSlice";
 import { Button, Spinner } from "react-bootstrap";
 import { Lightbulb, Linkedin } from "react-bootstrap-icons";
+import TopBar from "./Topbar";
 
 const Jobs = () => {
   const dispatch = useDispatch();
@@ -37,62 +38,65 @@ const Jobs = () => {
   }
 
   return (
-    <div className="container">
-      <h1>Jobs</h1>
-      <div className="row">
-        <div className="col-md-4">
-          <ul className="list-group">
-            {jobs.length > 0 ? (
-              jobs.map((job) => (
-                <li
-                  key={job._id}
-                  className={`list-group-item ${selectedJob && selectedJob._id === job._id ? "active" : ""}`}
-                  onClick={() => handleJobClick(job)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <h4>{job.title}</h4>
-                  <p>{job.company_name}</p>
-                  <p>{job.category}</p>
-                </li>
-              ))
-            ) : (
-              <li className="list-group-item">No jobs found.</li>
-            )}
-          </ul>
-        </div>
+    <>
+      <TopBar></TopBar>
+      <div className="container">
+        <h1>Jobs</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <ul className="list-group">
+              {jobs.length > 0 ? (
+                jobs.map((job) => (
+                  <li
+                    key={job._id}
+                    className={`list-group-item ${selectedJob && selectedJob._id === job._id ? "active" : ""}`}
+                    onClick={() => handleJobClick(job)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <h4>{job.title}</h4>
+                    <p>{job.company_name}</p>
+                    <p>{job.category}</p>
+                  </li>
+                ))
+              ) : (
+                <li className="list-group-item">No jobs found.</li>
+              )}
+            </ul>
+          </div>
 
-        <div className="col-md-8">
-          {selectedJob ? (
-            <div>
-              <h2>{selectedJob.title}</h2>
-              <p>{selectedJob.company_name}</p>
-              <p>{selectedJob.category}</p>
-              <p>{selectedJob.job_type}</p>
-              <p>
-                <Lightbulb /> Vedi un confronto con oltre 100 altri candidati.{" "}
-                <span>
-                  {" "}
-                  <p className="txtp">Prova Premium per 0 EUR</p>
-                </span>
-              </p>
-              <Button variant="primary rounded-pill mb-3">
-                <Linkedin className="mx-2 mb-1"></Linkedin>Candidatura semplice
-              </Button>
-              <Button variant="outline-primary rounded-pill mb-3 mx-3 px-3">
-                <strong>Salva</strong>
-              </Button>
-              <h4 className="mb-3">Informazioni sull'offerta di lavoro</h4>
-              <div dangerouslySetInnerHTML={{ __html: selectedJob.description }} />
-              <p>Salary: {selectedJob.salary}</p>
-              <h4>Usefull Link:</h4>
-              <Link>{selectedJob.url}</Link>
-            </div>
-          ) : (
-            <div>Select a job to see details.</div>
-          )}
+          <div className="col-md-8">
+            {selectedJob ? (
+              <div>
+                <h2>{selectedJob.title}</h2>
+                <p>{selectedJob.company_name}</p>
+                <p>{selectedJob.category}</p>
+                <p>{selectedJob.job_type}</p>
+                <p>
+                  <Lightbulb /> Vedi un confronto con oltre 100 altri candidati.{" "}
+                  <span>
+                    {" "}
+                    <p className="txtp">Prova Premium per 0 EUR</p>
+                  </span>
+                </p>
+                <Button variant="primary rounded-pill mb-3">
+                  <Linkedin className="mx-2 mb-1"></Linkedin>Candidatura semplice
+                </Button>
+                <Button variant="outline-primary rounded-pill mb-3 mx-3 px-3">
+                  <strong>Salva</strong>
+                </Button>
+                <h4 className="mb-3">Informazioni sull'offerta di lavoro</h4>
+                <div dangerouslySetInnerHTML={{ __html: selectedJob.description }} />
+                <p>Salary: {selectedJob.salary}</p>
+                <h4>Usefull Link:</h4>
+                <Link>{selectedJob.url}</Link>
+              </div>
+            ) : (
+              <div>Select a job to see details.</div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
